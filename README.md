96b626296adc3504e9e885b56d9bb699fff8a8d1
William Cui <444738577@qq.com>
2023-06-18 19:23:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<div style="width:100%">
+    <div style="width:50%; font-size: 24px">
+        <div align="center">
+          This is an instant chat application based on CometChat and React
+        </div>    
+    </div>    
+</div>
---
+++ src/defaultPages/App/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createBrowserHistory } from 'history';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+
+import PrivateRoute from '../../PrivateRoute';
+
+import Login from '../Login';
+
+import * as actions from '../../store/action';
+
+import { CometChatUI } from '../../cometchat-chat-uikit-react/CometChatWorkspace/src';
+
+import { wrapperStyle } from "./style";
+
+const history = createBrowserHistory();
+
+class App extends React.Component {
+    state = {
+        isLoggedin: false
+    }
+
+    componentDidMount() {
+        this.props.getLoggedinUser();
+    }
+
+    render() {
+
+        return (
+            <div css={wrapperStyle()}>
+                <Router history={history}>
+                    <Switch>
+                        <PrivateRoute exact path="/" component={CometChatUI} />
+                        <Route path="/login" component={Login} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getLoggedinUser: () => dispatch(actions.authCheckState())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
